642abd659d9b536db75b58fa28254911fa5ba821
mateu52 <wuoelte@gmail.com>
2025-11-17 20:54:27 +0100

    data annotations

--- Dto/CreateProductDto.cs
+++ Dto/CreateProductDto.cs
@@ -1,10 +1,21 @@
-﻿namespace wmsmagazyn.Dto
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace wmsmagazyn.Dto
 {
     public class CreateProductDto
     {
-        public string Name { get; set; }
+        [Required(ErrorMessage = "Nazwa produktu jest wymagana")]
+        [StringLength(100, ErrorMessage = "Nazwa produktu może mieć maksymalnie 100 znaków")]
+        public string Name { get; set; } = null!;
+
+        [Required(ErrorMessage = "Cena jest wymagana")]
+        [Range(0.01, 100000, ErrorMessage = "Cena musi być większa niż 0")]
         public decimal Price { get; set; }
-        public string Barcode { get; set; }
-        public string Unit { get; set; }
+
+        [Required(ErrorMessage = "Kod kreskowy jest wymagany")]
+        public string Barcode { get; set; } = null!;
+
+        [Required(ErrorMessage = "Jednostka jest wymagana")]
+        public string Unit { get; set; } = null!;
     }
 }
--- Dto/LoginDto.cs
+++ Dto/LoginDto.cs
@@ -1,8 +1,14 @@
-﻿namespace wmsmagazyn.Dto
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace wmsmagazyn.Dto
 {
     public class LoginDto
     {
-        public string? Name { get; set; }
-        public string? Password { get; set; }
+        [Required(ErrorMessage = "Nazwa użytkownika jest wymagana")]
+        public string Name { get; set; } = null!;
+
+        [Required(ErrorMessage = "Hasło jest wymagane")]
+        [StringLength(50, MinimumLength = 4, ErrorMessage = "Hasło musi mieć od 4 do 50 znaków")]
+        public string Password { get; set; } = null!;
     }
 }
